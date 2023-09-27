d0eb0b6aab72a01c9e3a8e0ce68c984611446995
akash <akashsonkamble199@gmail.com>
2023-09-28 00:56:18 +0530

    refactor(components): Update Button, Footer, Header, Input, Logo, Container, index components

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+const Logo = ({ width = "100px" }) => {
+  return <div>Logo</div>;
+};
 
-const Logo = ({ width = '100px' }) => {
-  return (
-    <div>Logo</div>
-  )
-}
-
-export default Logo
+export default Logo;
