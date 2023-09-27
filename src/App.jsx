d854c7a3d237e5b6de285504df509044fe92df3a
akash <akashsonkamble199@gmail.com>
2023-09-27 21:19:21 +0530

    feat(app): Create Container, index, Header, Footer, Logo components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
 import './App.css'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
@@ -26,9 +29,13 @@ function App() {
 
   return !loading ? (
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
-      <header className="w-full block">
-        
-      </header>
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   )
   : null
