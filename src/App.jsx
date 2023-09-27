cb75074cf1786f309fbcd6c9abc445350491498a
akash <akashsonkamble199@gmail.com>
2023-09-27 20:47:05 +0530

    refactore(app): Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 import './App.css'
+
 
 function App() {
 
-  return (
-    <>
-      <h1>A blog app with Appwrite</h1>
-    </>
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then(userData => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [dispatch])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <header className="w-full block">
+        
+      </header>
+    </div>
   )
+  : null
 }
 
 export default App
