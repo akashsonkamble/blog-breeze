0ae9340123ff20f10cc3fea4c6602982dd38dc1f
akash <akashsonkamble199@gmail.com>
2023-09-29 01:54:18 +0530

    feat(components): Create AuthLayout, Login, PostCard and Signup components

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+
+const Protected = ({ children, authentication = true}) => {
+    const navigate = useNavigate();
+    const authStatus = useSelector(state => state.auth.status);
+
+    const [loader, setLoader] = useState(true);
+
+    useEffect(() => {
+        if (!authentication && authStatus !== authentication) {
+            navigate("/login");
+        } else if (authentication && authStatus === authentication) {
+            navigate("/");
+        }
+        
+        setLoader(false);
+    }, [authStatus, navigate, authentication]);
+  return loader ? <p>Loading...</p> : <>{children}</>
+}
+
+export default Protected;
