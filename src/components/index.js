1909be3b15259a0439a9fe2e79c081643d8a67e1
akash <akashsonkamble199@gmail.com>
2023-10-02 14:51:49 +0530

    refactor(components): Update PostCard component and index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo"
+import Select from "./Select"
+import RTE from "./RTE"
 
 export {
     Header,
     Footer,
     Container,
     LogoutBtn,
-    Logo
+    Logo,
+    Select,
+    RTE
 }
