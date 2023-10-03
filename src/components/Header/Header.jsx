e96ea0b4c1e66a13f5351468f54cb2ff66f33f0d
akash <akashsonkamble199@gmail.com>
2023-10-03 21:31:41 +0530

    style(components): Update styles in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
                 <li key={item.name}>
                   <Button
                     onClick={() => navigate(item.slug)}
-                    className="inline-block mr-2 px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    className="inline-block mr-1 px-6 py-2 duration-200 rounded-full"
                   >
                     {item.name} 
                   </Button>
