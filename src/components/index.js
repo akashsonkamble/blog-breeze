0af1d7dd864ca28a47997073651442ab0fd83dff
akash <akashsonkamble199@gmail.com>
2023-10-06 09:24:18 +0530

    fix(index): Correct import statement for Container component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container/Container";
+import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo"
 import Select from "./Select"
