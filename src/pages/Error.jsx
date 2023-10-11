74d0aa78f516b0de31d77dfa297ccb6811fced09
akash <akashsonkamble199@gmail.com>
2023-10-11 21:18:38 +0530

    refactor(ErrorPage): Improve code in ErrorPage components

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,23 +1,34 @@
-import { Header, Footer, Container } from "../components";
+import { Footer, Container, Logo, Button } from "../components";
+
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <>
-            <Header />
-            <div className="w-full py-8 mt-4 text-center">
+            <header className="py-3 bg-#d2e2f2 border-b-2 border-500">
                 <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold">
-                                An error occurred!
-                            </h1>
-                            &nbsp;
-                            <p className="text-lg">
-                                Could not find the page you were looking for.
-                            </p>
-                        </div>
-                    </div>
+                <div className="mr-4">
+                    <Link to="/">
+                        <Logo width="150px" />
+                    </Link>
+                </div>
                 </Container>
+            </header>
+            <div className="w-full py-8 mt-4 text-center">
+                <div className="flex flex-wrap">
+                    <div className="p-2 w-full">
+                        <h1 className="text-4xl font-bold">
+                                Something went wrong!
+                        </h1>
+                        &nbsp;
+                        <p className="text-lg">
+                            Could not find the page you were looking for.
+                        </p>
+                        <Button className="mt-6">
+                            <Link to="/">Back to Home</Link>
+                        </Button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
