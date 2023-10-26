e13594e134a0b007c3069265afffce535602d9c7
akash <akashsonkamble199@gmail.com>
2023-10-26 19:06:13 +0530

    Refactor(RTE): Remove unnecessary code

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 import { Controller } from "react-hook-form";
 import { Editor } from "@tinymce/tinymce-react";
 import { toast } from "react-toastify";
